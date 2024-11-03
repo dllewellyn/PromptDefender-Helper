499fec187b4f28a0bfddf7820d1890d87b775954
dllewellyn <danieln.llewellyn@gmail.com>
2024-11-03 20:07:21 +0000

    Adding an API endpoint for improve

--- endpoints/improve.go
+++ endpoints/improve.go
@@ -11,34 +11,62 @@ import (
 
 func AddImprover(ctx context.Context, engine *gin.Engine, improver improve.Improver, promptCache cache.Cache) {
 	engine.POST("/improve", func(c *gin.Context) {
-
-		prompt := c.PostForm("prompt")
-
-		cachedResponse, err := promptCache.Get(ctx, prompt+"_improve")
+		prompt, err := getPromptFromRequest(c)
 
 		if err != nil {
-			c.Redirect(http.StatusOK, "/error")
+			handleError(c, http.StatusBadRequest, "/error")
+			return
+		}
+
+		cachedResponse, err := promptCache.Get(ctx, prompt+"_improve")
+		if err != nil {
+			handleError(c, http.StatusOK, "/error")
 			return
 		}
 
 		if cachedResponse != "" {
-			c.HTML(http.StatusOK, "improve.html", cachedResponse)
+			handleResponse(c, cachedResponse)
 			return
 		}
 
 		response, err := improver.Improve(prompt)
-
 		if err != nil {
 			log.Println(err)
-			c.Redirect(http.StatusOK, "/error")
+			handleError(c, http.StatusOK, "/error")
+			return
 		}
 
 		err = promptCache.Set(ctx, prompt+"_improve", response)
-
 		if err != nil {
 			log.Println(err)
-			c.Redirect(http.StatusOK, "/error")
+			handleError(c, http.StatusOK, "/error")
+			return
 		}
-		c.HTML(http.StatusOK, "improve.html", response)
+
+		handleResponse(c, response)
 	})
+}
+
+func handleResponse(c *gin.Context, response string) {
+	if c.GetHeader("Content-Type") == "application/json" {
+		c.JSON(http.StatusOK, gin.H{"response": response})
+	} else {
+		c.HTML(http.StatusOK, "improve.html", response)
+	}
+}
+
+func getPromptFromRequest(c *gin.Context) (string, error) {
+	contentType := c.GetHeader("Content-Type")
+	if contentType == "application/json" {
+		var jsonData map[string]string
+		if err := c.ShouldBindJSON(&jsonData); err != nil {
+			return "", err
+		}
+		return jsonData["prompt"], nil
+	}
+	return c.PostForm("prompt"), nil
+}
+
+func handleError(c *gin.Context, statusCode int, redirectURL string) {
+	c.Redirect(statusCode, redirectURL)
 }
